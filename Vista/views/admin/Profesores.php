c715f9f83ce0a273d37d567a69ff027a50df6f3c
FauricioCespedes <fauriciocespedes@gmail.com>
2022-07-18 08:37:31 -0600

    actualización

--- Vista/views/admin/Profesores.php
+++ Vista/views/admin/Profesores.php
@@ -14,11 +14,81 @@
 		<?php echo $header; ?>
 		<main class="d-flex">
 		<?php echo $sidebar; ?>
-				<section class="content-section">
-						
+				<section class="content-section w-75 mx-auto">
+						<h1 class="fs-3 mt-2">Profesores</h1> 						
+						<table
+						id="tableProfesores"
+						data-toggle="table"
+						data-height="600"
+						data-show-toggle = "true"
+						data-pagination="true"
+						data-search-align="left"
+						data-page-list="[10, 25, 50, 100, all]"
+						data-buttons="buttons"
+						data-mobile-responsive= "true"
+						data-check-on-init= "true"
+						data-search="true"
+						data-url="">
+						<thead>
+								<tr>
+										<th data-checkbox="true" data-sortable="true"></th>
+										<th data-field="name" data-sortable="true">Nombre</th>
+										<th data-field="primerapellido data-sortable="true"">Apellidos</th>
+										<th data-field="cedula data-sortable="true"">Cedula</th>
+										<th data-field="saldo data-sortable="true"">Saldo</th>
+										<th data-field="modificar data-sortable="true"">Editar</th>
+								</tr>
+						</thead>
+						<tbody>
+								<tr>
+										<td></td>
+										<td>Pepe</td>
+										<td>Castro</td>
+										<td>40555</td>
+										<td>$1234</td>
+										<td>boton</td>
+								</tr>
+								<tr>
+										<td></td>
+										<td>Juan</td>
+										<td>Castro</td>
+										<td>30555</td>
+										<td>$1234</td>
+										<td>boton</td>
+								</tr>
+						</tbody>
+						</table>
 				</section>
 		</main>
 		<?php echo $footer; ?>
+		<script>
+				function buttons (){
+						return{
+								btnCrear: {
+										text: 'Crear Pofesor',
+										icon: 'bi-plus-circle',
+										event: function () {
+												location.href = './?';
+										},
+										attributes: {
+											title: 'Crear Profesor',
+										  style: 'background-color: #5e931a;'
+										}
+								},
+								btnEliminar: {
+										text: 'Eliminar Profesor',
+										icon: 'bi-trash',
+										event: function () {
+												location.href = './?';
+										},
+										attributes: {
+											title: 'Eliminar Profesor',
+										  style: 'background-color: #a61717;'
+										}
+								}
+						}
+				}	
+		</script>
 </body>
 </html>
 
--- Vista/views/components/Head.php
+++ Vista/views/components/Head.php
@@ -26,5 +26,12 @@
 <!-- Bootstrap JS -->
 <script defer src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 
+<!-- Bootstrap Table -->
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css">
+<link rel="stylesheet" href="https://unpkg.com/bootstrap-table@1.20.2/dist/bootstrap-table.min.css">
+<script defer src="https://unpkg.com/bootstrap-table@1.20.2/dist/bootstrap-table.min.js"></script>
+<script src="https://unpkg.com/bootstrap-table@1.20.2/dist/extensions/mobile/bootstrap-table-mobile.min.js"></script>
+<script src="extensions/mobile/bootstrap-table-mobile.js"></script>
+
 <!-- CSS -->
 <link rel="stylesheet" href="./Vista/css/styles.css">
