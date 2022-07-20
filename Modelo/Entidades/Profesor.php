1cd99b29b2e8d5057e673c56a37fb7ebc08be656
Jeferson Fuentes <jeferson.fg710@gmail.com>
2022-07-20 08:20:34 -0600

    Entidades Profesor, actualizado

--- Modelo/Entidades/Profesor.php
+++ Modelo/Entidades/Profesor.php
@@ -1,85 +1,104 @@
 <?php
-class Profesor{
-		private $id;
-		private $nombre;
-		private $primerApellido;
-		private $segundoApellido;
-		private $cedula;
-		private $saldo;
-		private $estado;
-		private $contrasena;
-		private $correo;
+class Profesor
+{
+	private $id;
+	private $nombre;
+	private $primerApellido;
+	private $segundoApellido;
+	private $cedula;
+	private $saldo;
+	private $estado;
+	private $contrasena;
+	private $correo;
 
-		public function getId(){
-			return $this->id;
-		}
+	public function getId()
+	{
+		return $this->id;
+	}
 
-		public function setId($id){
-			$this->id = $id;
-		}
+	public function setId($id)
+	{
+		$this->id = $id;
+	}
 
-		public function getNombre(){
-			return $this->nombre;
-		}
+	public function getNombre()
+	{
+		return $this->nombre;
+	}
 
-		public function setNombre($nombre){
-			$this->nombre = $nombre;
-		}
+	public function setNombre($nombre)
+	{
+		$this->nombre = $nombre;
+	}
 
-		public function getPrimerApellido(){
-			return $this->primerApellido;
-		}
+	public function getPrimerApellido()
+	{
+		return $this->primerApellido;
+	}
 
-		public function setPrimerApellido($primerApellido){
-			$this->primerApellido = $primerApellido;
-		}
+	public function setPrimerApellido($primerApellido)
+	{
+		$this->primerApellido = $primerApellido;
+	}
 
-		public function getSegundoApellido(){
-			return $this->segundoApellido;
-		}
+	public function getSegundoApellido()
+	{
+		return $this->segundoApellido;
+	}
 
-		public function setSegundoApellido($segundoApellido){
-			$this->segundoApellido = $segundoApellido;
-		}
+	public function setSegundoApellido($segundoApellido)
+	{
+		$this->segundoApellido = $segundoApellido;
+	}
 
-		public function getCedula(){
-			return $this->cedula;
-		}
+	public function getCedula()
+	{
+		return $this->cedula;
+	}
 
-		public function setCedula($cedula){
-			$this->cedula = $cedula;
-		}
+	public function setCedula($cedula)
+	{
+		$this->cedula = $cedula;
+	}
 
-		public function getSaldo(){
-			return $this->saldo;
-		}
+	public function getSaldo()
+	{
+		return $this->saldo;
+	}
 
-		public function setSaldo($saldo){
-			$this->saldo = $saldo;
-		}
+	public function setSaldo($saldo)
+	{
+		$this->saldo = $saldo;
+	}
 
-		public function getEstado(){
-			return $this->estado;
-		}
+	public function getEstado()
+	{
+		return $this->estado;
+	}
 
-		public function setEstado($estado){
-			$this->estado = $estado;
-		}
+	public function setEstado($estado)
+	{
+		$this->estado = $estado;
+	}
 
 
-		public function getCorreo(){
-			return $this->correo;
-		}
+	public function getCorreo()
+	{
+		return $this->correo;
+	}
 
-		public function setCorreo($correo){
-			$this->correo = $correo;
-		}
+	public function setCorreo($correo)
+	{
+		$this->correo = $correo;
+	}
 
-		public function getContrasena(){
-			return $this->contrasena;
-		}
+	public function getContrasena()
+	{
+		return $this->contrasena;
+	}
 
-		public function setContrasena($contrasena){
-			$this->contrasena = $contrasena;
-		}
+	public function setContrasena($contrasena)
+	{
+		$this->contrasena = $contrasena;
+	}
 }
