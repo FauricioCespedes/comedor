fe5fa15aff88772c55e67593cc96246e37a2da47
Jeferson Fuentes <jeferson.fg710@gmail.com>
2022-07-20 09:09:59 -0600

    Login vacio

--- Vista/views/Login.php
+++ Vista/views/Login.php
@@ -1,13 +1,21 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-		<?php
-				$head = file_get_contents('./Vista/views/components/Head.php');
-				echo $head;
-		?>
-    <title>Login</title>
+	<?php
+	$head = file_get_contents('./Vista/views/components/Head.php');
+	echo $head;
+	?>
+	<title>Login</title>
 </head>
+
 <body>
+	
+
+	<!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
+	<script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous"></script>
 
 </body>
+
 </html>
